1c4f143af55ab0a4100290761842c901be19650a
Qolizade <qolizade151@hotmail.com>
2022-12-29 18:05:19 +0330

    add Event liteners

--- script.js
+++ script.js
@@ -4,4 +4,10 @@ const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector("todo-list");
 const filterOption = document.querySelector("filter-todo");
 
+// Event listeners
+document.addEventListener("DOMContentLoaded", getTodos);
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
+
 
