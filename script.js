3a638bd1196a0ba625b94b5fd5509cae6aa19b61
Qolizade <qolizade151@hotmail.com>
2022-12-29 18:00:52 +0330

    add selectors

---
+++ script.js
@@ -0,0 +1,7 @@
+// selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector("todo-list");
+const filterOption = document.querySelector("filter-todo");
+
+
