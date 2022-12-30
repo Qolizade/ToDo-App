ea021737d8f4913ca84a4e50280b9c86b57e085c
Qolizade <qolizade151@hotmail.com>
2022-12-30 17:25:45 +0330

    remove local storage

--- script.js
+++ script.js
@@ -95,6 +95,16 @@ function saveLocalTodos(todo) {
   localStorage.setItem("todos", JSON.stringify(todos));
 }
 
-function removeLocalTodos(todo) {}
+function removeLocalTodos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  const todoIndex = todo.children[0].innerText;
+  todos.splice(todos.indexOf(todoIndex), 1);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
 
 function getTodos() {}
